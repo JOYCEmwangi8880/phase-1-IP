c132b9777d60c80702455883eb95a1e16aa0651f
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2023-10-12 18:31:45 +0000

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # phase-1-IP
+# Charisma: A Dungeons & Dragons (D&D) Learning Experience
+
+## Table of Contents
+
+- [Project Overview](#project-overview)
+- [Technologies Used](#technologies-used)
+- [Key Features](#key-features)
+- [Installation](#installation)
+- [Contact](#contact)
+
+## Project Overview
+
+"Charisma" is a web application that explores the world of Dungeons & Dragons (D&D) and how it can provide valuable life lessons. D&D is a popular tabletop role-playing game known for fostering creativity, teamwork, and problem-solving skills. In "Charisma," we focus on the Charisma attribute and its associated skills: Deception, Intimidation, Performance, and Persuasion.
+
+This project aims to highlight the life lessons that can be learned from playing D&D, especially in terms of teamwork, problem-solving, and the importance of diversity within a group.
+
+
+
+## Technologies Used
+
+"Charisma" was built using the following technologies:
+
+- **HTML:** The structure and content of the web pages.
+- **CSS:** Styling and layout of the web pages.
+- **Javacript:** Dynamic functionality, including skill descriptions and interactive features.
+- **D&D 5e API:** To retrieve skill descriptions from the D&D API.
+
+## Key Features
+
+"Charisma" offers several key features to enhance your D&D learning experience:
+
+- **Learn About Charisma:** Discover the Charisma attribute and its associated skills.
+- **Life Lessons:** Explore valuable life lessons that can be learned from playing D&D.
+- **Interactive Skills:** Interact with the skills to view their descriptions and learn more about them.
+- **Likes and Comments:** Express your interest by liking and commenting on specific skills.
+- **Community Engagement:** Engage in discussions with others who share your interest in D&D and Charisma.
+
+## Installation
+
+To run "Charisma" locally on your machine, follow these steps:
+
+1. Clone the repository to your local machine:
+
+## Contacts 
+Email: joycewanguim2020@gmail.com
+ ## MY Github Page
+ link: https://github.com/JOYCEmwangi8880
+
+ ## LinkedIn
+ link: linkedin.com/in/joyce-wangui-470019235
+
---
+++ script.js
@@ -0,0 +1,117 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Define skill buttons and their corresponding API endpoints
+    const skillButtons = document.querySelectorAll('.skill-button');
+    const skillEndpoints = {
+      Deception: '/api/skills/deception',
+      Intimidation: '/api/skills/intimidation',
+      Performance: '/api/skills/performance',
+      Persuasion: '/api/skills/persuasion'
+    };
+
+    // Store like counts and comments for each skill
+    const likeCounts = {
+      Deception: 0,
+      Intimidation: 0,
+      Performance: 0,
+      Persuasion: 0
+    };
+
+    const comments = {
+      Deception: [],
+      Intimidation: [],
+      Performance: [],
+      Persuasion: []
+    };
+
+    // Event listener for the "like" button
+    const likeButtons = document.querySelectorAll('.like-button');
+    likeButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const skillName = button.getAttribute('data-skill');
+        const likeCount = button.nextElementSibling;
+        likeCounts[skillName]++;
+        likeCount.textContent = likeCounts[skillName];
+      });
+    });
+
+    // Event listener for comment submission
+    const commentSubmitButtons = document.querySelectorAll('.comment-submit');
+    commentSubmitButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const skillName = button.getAttribute('data-skill');
+        const commentInput = button.previousElementSibling;
+        const commentText = commentInput.value;
+        comments[skillName].push(commentText);
+        commentInput.value = ''; // Clear the input field
+        displayComments(skillName); // Call the function to display comments
+      });
+    });
+
+    // Function to display comments for a skill
+    function displayComments(skillName) {
+      const commentsForSkill = comments[skillName];
+      const commentsContainer = document.getElementById('comments');
+      commentsContainer.innerHTML = ''; // Clear the comments container
+
+      commentsForSkill.forEach(comment => {
+        const commentDiv = document.createElement('div');
+        commentDiv.textContent = comment;
+        commentsContainer.appendChild(commentDiv);
+      });
+    }
+
+    // Add click event listeners to skill buttons
+    skillButtons.forEach(button => {
+      // Create a container for the skill description
+      const descriptionContainer = document.createElement('div');
+      descriptionContainer.classList.add('skill-description');
+      button.appendChild(descriptionContainer);
+
+      button.addEventListener('click', () => {
+        const skillName = button.textContent;
+        const skillEndpoint = skillEndpoints[skillName];
+
+        if (skillEndpoint) {
+          fetch(`https://www.dnd5eapi.co${skillEndpoint}`)
+            .then(response => response.json())
+            .then(data => {
+              const skillDescription = data.desc[0];
+
+              if (skillDescription) {
+                descriptionContainer.textContent = skillDescription;
+              } else {
+                descriptionContainer.textContent = 'Skill description not found.';
+              }
+            })
+            .catch(error => {
+              console.error('Error:', error);
+            });
+        } 
+      });
+    });
+  });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName('lesson-slide');
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slides[slideIndex - 1].style.display = 'block';
+}
